968fd784f260c9b28becf8a6fac863b685698c2d
DanielSoaresRocha <legoliga123@gmail.com>
2020-07-10 16:44:27 -0300

    fazendo animação no números

--- src/js/main.js
+++ src/js/main.js
@@ -32,3 +32,56 @@ function listeners () {
 }
 
 listeners()
+
+async function incrementNumbers () {
+    function sleep (ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    const totalSales = document.getElementById('totalSales')
+    const totalIncome = document.getElementById('totalIncome')
+    const totalViews = document.getElementById('totalViews')
+
+    const qtdSales = parseInt(totalSales.textContent, 10)
+    const qtdIncome = parseInt(totalIncome.textContent, 10)
+    const qtdViews = parseInt(totalViews.textContent, 10)
+
+    let currentSales = 0;
+    let currentIncome = 0;
+    let currentViews = 0;
+
+    for (let i = 0; i < 1000; i++) {
+        // CURRENT SALES
+        if (currentSales + 150 <= qtdSales) {
+            currentSales = currentSales + 150
+        } else if (currentSales + 1 <= qtdSales) {
+            currentSales++
+        }
+
+        totalSales.innerHTML = `${currentSales}`
+
+        // TOTAL INCOME
+        if (currentIncome + 1000 <= qtdIncome) {
+            currentIncome = currentIncome + 1000
+        } else if (currentIncome + 50 <= qtdIncome) {
+            currentIncome += 50
+        } else if (currentIncome + 1 <= qtdIncome) {
+            currentIncome++
+        }
+        totalIncome.innerHTML = `${currentIncome.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`
+
+        // TOTAL VIEWS
+        if (currentViews + 5000 <= qtdViews) {
+            currentViews = currentViews + 5000
+        } else if (currentViews + 50 <= qtdViews) {
+            currentViews += 50
+        } else if (currentViews + 1 <= qtdViews) {
+            currentViews++
+        }
+        totalViews.innerHTML = `${currentViews}`
+
+        await sleep(0.001)
+    }
+}
+
+incrementNumbers()
